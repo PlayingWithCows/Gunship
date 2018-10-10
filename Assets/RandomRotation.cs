9928d4a2f6552c61fbfd8fd6c1c5572e72abc8b0
PlayingWithCows <wiealbern@gmail.com>
2018-10-10 20:40:30 +0200

    random rotation

---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+    Vector3 rotation;
+    private void Start()
+    {
+        rotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    }
+    private void Update()
+    {
+        transform.Rotate(rotation.x, rotation.y, rotation.z);
+    }
+
+}
