c6e132018b3163f5a651a5fc2289eb8f2e143e65
PlayingWithCows <wiealbern@gmail.com>
2018-10-19 15:26:49 +0200

    Enemy spawning prepared

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Player player;
+    public float speed;
+    public float turnSpeed;
+
+    public Vector3 destination;
+    private float currentSpeed;
+
+
+    private void Start()
+    {
+        Turn();
+        Move();
+    }
+
+    private void Turn()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+        transform.Translate(0, currentSpeed/100, 0);
+    }
 }
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public Vector3 distanceToPlayer;
+    public Player player;
+    public bool triggered;
+    public GameObject[] shipsToSpawn;
+    public int spawnIndex = 0;
+    public int enemiesToSpawn = 0;
+    public float spawnRate = 0.5f;
+    private Vector3 spawnPos;
+    private float timeLastSpawned = 0;
+   
+
+    // Use this for initialization
+    void Start () {
+        enemiesToSpawn = shipsToSpawn.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (triggered && timeLastSpawned + spawnRate < Time.time && spawnIndex < enemiesToSpawn )
+        {
+            SpawnEnemy();
+        }
+	}
+    void SpawnEnemy()
+    {
+       spawnPos = player.transform.position + distanceToPlayer;
+       Instantiate(shipsToSpawn[spawnIndex], spawnPos, Quaternion.identity, transform);
+       spawnIndex++;
+
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Player>() != null)
+        {
+            player = col.GetComponent<Player>();
+            triggered = true;
+            distanceToPlayer = transform.position - col.transform.position;
+        }
+    }
+}
